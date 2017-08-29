77433571c11c3a0b82a107d5dd82d20c85dc1431
Joonas <joonas.pitkonen@hotmail.fi>
2017-08-29 15:38:09 +0300

    rojektin luonti

---
+++ Dictio/app/src/main/java/com/example/a1500889/dictio/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a1500889.dictio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
