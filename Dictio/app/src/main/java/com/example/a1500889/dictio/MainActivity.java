0525f6fd7a349e0eda46f775e4aa34e6c4e397ce
Sierra-250 <a1500908@myy.haaga-helia.fi>
2017-08-30 10:41:20 +0300

    Menu, selecttheme, theme1

--- Dictio/app/src/main/java/com/example/a1500889/dictio/MainActivity.java
+++ Dictio/app/src/main/java/com/example/a1500889/dictio/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.a1500889.dictio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,13 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button1 = (Button) findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                TextView textView1 = (TextView) findViewById(R.id.textView1);
-                textView1.setVisibility(View.VISIBLE);
-            }
-        });
+        //final Button button1 = (Button) findViewById(R.id.button1);
+        //button1.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View view) {
+        //        TextView textView1 = (TextView) findViewById(R.id.textView1);
+        //        textView1.setVisibility(View.VISIBLE);
+        //    }
+        //});
+    }
+
+    public void gotoSelectTheme(View view){
+        Intent intent = new Intent(this, selectThemeActivity.class);
+        startActivity(intent);
     }
 }
--- Dictio/app/src/main/java/com/example/a1500889/dictio/theme1.java
+++ Dictio/app/src/main/java/com/example/a1500889/dictio/theme1.java
@@ -5,18 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class selectThemeActivity extends AppCompatActivity {
+public class theme1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_selecttheme);
+        setContentView(R.layout.activity_theme);
 
-    }
-
-    public void gotoTheme1(View view){
-        Intent intent = new Intent(this, theme1.class);
-        startActivity(intent);
     }
 
 }
