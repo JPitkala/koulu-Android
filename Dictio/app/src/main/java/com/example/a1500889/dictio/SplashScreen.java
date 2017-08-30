c751743029623963a2e848175f0f0ddbc48f899e
Pitkälä Jaakko <a1500949@myy.haaga-helia.fi>
2017-08-30 10:31:19 +0300

    Splash screen

---
+++ Dictio/app/src/main/java/com/example/a1500889/dictio/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.example.a1500889.dictio;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+
+    private static int SPLASH_TIME_OUT = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+
+        }
+
+    }
+
