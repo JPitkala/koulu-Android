47d9c1cf0aed0e68b6ad748c44c189b1b54d2717
Joonas <joonas.pitkonen@hotmail.fi>
2017-09-02 15:29:11 +0300

    splash screen ja yleiset ulkoasu muokkaukset

--- Dictio/app/src/main/java/com/example/a1500889/dictio/SplashScreen.java
+++ Dictio/app/src/main/java/com/example/a1500889/dictio/SplashScreen.java
@@ -5,6 +5,7 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -16,6 +17,7 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splashscreen);
 
         new Handler().postDelayed(new Runnable() {
