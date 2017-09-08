c4bc112a08bddcb5673495bae88d04282b1471ec
Jaakko <a1500949@myy.haaga-helia.fi>
2017-09-08 19:19:43 +0300

    Tekstimuutoksia

--- Dictio/app/src/main/java/com/example/a1500889/dictio/selectThemeActivity.java
+++ Dictio/app/src/main/java/com/example/a1500889/dictio/selectThemeActivity.java
@@ -5,45 +5,50 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class selectThemeActivity extends AppCompatActivity {
 
 
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_selecttheme);
+    setContentView(R.layout.activity_selecttheme);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        text = (TextView)findViewById(R.id.textView);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new         Intent(getApplicationContext(),MainActivity.class));
-            }
+                Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+                setSupportActionBar(toolbar);
+
+                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+                toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View v) {
+        startActivity(new         Intent(getApplicationContext(),MainActivity.class));
+        }
         });
 
-    }
+        }
 
 
-    public void gotoTheme1(View view){
+public void gotoTheme1(View view){
         Intent intent = new Intent(this, theme1.class);
         startActivity(intent);
-    }
+        }
 
-    public void gotoSpell(View view){
+public void gotoSpell(View view){
         Intent intent2 = new Intent(this, theme2.class);
         startActivity(intent2);
-    }
+        }
 
-    public void gotoTheme3(View view){
+public void gotoTheme3(View view){
         Intent intent3 = new Intent(this, theme3.class);
         startActivity(intent3);
-    }
+        }
 
-}
+        }
