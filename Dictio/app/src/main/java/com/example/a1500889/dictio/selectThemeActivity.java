47e6ddf8e1c19b2611f32cf9b6e7f8a13fb463bf
Joonas <joonas.pitkonen@hotmail.fi>
2017-09-18 09:31:23 +0300

    puuttuva kuva

--- Dictio/app/src/main/java/com/example/a1500889/dictio/selectThemeActivity.java
+++ Dictio/app/src/main/java/com/example/a1500889/dictio/selectThemeActivity.java
@@ -1,6 +1,7 @@
 package com.example.a1500889.dictio;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
