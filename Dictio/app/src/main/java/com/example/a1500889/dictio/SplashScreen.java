b4015738bd188a3f7bac9917208c3e0570b7989c
Joonas <joonas.pitkonen@hotmail.fi>
2017-09-25 15:08:54 +0300

    UI kuntoon isommilla ruuduilla, splash screeni kokoruudulle

--- Dictio/app/src/main/java/com/example/a1500889/dictio/SplashScreen.java
+++ Dictio/app/src/main/java/com/example/a1500889/dictio/SplashScreen.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -18,6 +19,9 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //Splash screenin kokoruudulle muuttaminen kahdella rivilla
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splashscreen);
 
         new Handler().postDelayed(new Runnable() {
